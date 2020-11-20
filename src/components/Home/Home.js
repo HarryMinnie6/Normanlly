8731b1ec04840d01b52edf6332e807464eecc9ee
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2020-11-20 06:13:32 +0200

    added orange corner markers

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -38,13 +38,20 @@ function Home() {
         <i className="fab fa-instagram"></i>
         <i className="fab fa-linkedin-in"></i>
       </div>
-      <div className="logo__wrapper"></div>
       <Link to="/about" className="next-page__right">
         <i className="fas fa-chevron-right"></i>
       </Link>
       <div className="home-page__wrapper">
-        <h1 className="header-1">Concept</h1>
-        <h1 className="header-2">Web Design</h1>
+        <div className="header-1__wrapper ">
+          <h1>Concept</h1>
+          <div className="color-div1"></div>
+          <div className="color-div2"></div>
+        </div>
+        <div className="header-2__wrapper ">
+          <h1>Web design</h1>
+          <div className="color-div3"></div>
+          <div className="color-div4"></div>
+        </div>
 
         <p className="paragraph-1">
           Creating beautifully designed, user-friendly websites that reflect
