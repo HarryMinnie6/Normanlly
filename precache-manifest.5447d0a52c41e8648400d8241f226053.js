e2c54c2892b5183eaba537f9e9920ac109db47d9
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-06-05 09:49:35 +0200

    Updates

---
+++ precache-manifest.5447d0a52c41e8648400d8241f226053.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b203acfbbee885c4247599c079686e",
+    "url": "/Normanlly/index.html"
+  },
+  {
+    "revision": "abba027ae015c9b5d2a3",
+    "url": "/Normanlly/static/css/main.97f40b9e.chunk.css"
+  },
+  {
+    "revision": "8993ce73a9feacdba64c",
+    "url": "/Normanlly/static/js/2.60127309.chunk.js"
+  },
+  {
+    "revision": "829c96705fc3ce5f8f1ed66bf6148a78",
+    "url": "/Normanlly/static/js/2.60127309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abba027ae015c9b5d2a3",
+    "url": "/Normanlly/static/js/main.8f51867e.chunk.js"
+  },
+  {
+    "revision": "04b88bb30b0fd6b63a54",
+    "url": "/Normanlly/static/js/runtime-main.e73fe333.js"
+  },
+  {
+    "revision": "f95cb8069c85577a7c84ea278e445a1f",
+    "url": "/Normanlly/static/media/Normanlly_Logo.f95cb806.jpg"
+  },
+  {
+    "revision": "1ea9beb715ded502b598c1508de74720",
+    "url": "/Normanlly/static/media/Normanlly_name.1ea9beb7.JPG"
+  },
+  {
+    "revision": "92622165d2f6b22175eb471cbd8bdec1",
+    "url": "/Normanlly/static/media/flex.92622165.png"
+  },
+  {
+    "revision": "272f75e041cc3a5b231325b1129d2011",
+    "url": "/Normanlly/static/media/menu.272f75e0.svg"
+  },
+  {
+    "revision": "afc068bed7dcb721ac849668c4322490",
+    "url": "/Normanlly/static/media/merge.afc068be.png"
+  },
+  {
+    "revision": "c74bce3c9bb78ed295ea11171eae8bcc",
+    "url": "/Normanlly/static/media/paving.c74bce3c.png"
+  },
+  {
+    "revision": "585302c9000741daa7919829cc66f91d",
+    "url": "/Normanlly/static/media/pexels-notebook.585302c9.jpg"
+  },
+  {
+    "revision": "e7506581fd021a5d8af36f30569f9b67",
+    "url": "/Normanlly/static/media/secondary-logo.e7506581.svg"
+  },
+  {
+    "revision": "3434ac7fec0514bfcee053b40289a719",
+    "url": "/Normanlly/static/media/struisbaai_accommodation.3434ac7f.png"
+  },
+  {
+    "revision": "61e1229de043f02694f89f58ec172e21",
+    "url": "/Normanlly/static/media/x.61e1229d.svg"
+  }
+]);
